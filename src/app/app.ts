493968ab1466475df417c2503fb8eded2d6c24c0
Mohamed Hamdy <99550272+MohamedHamada13@users.noreply.github.com>
2026-03-17 00:59:21 +0200

    Feat: build a product-card feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Slider } from './components/slider/slider';
+import { ProductCard } from './components/product-card/product-card';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Slider],
+  imports: [Navbar, Slider, ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
