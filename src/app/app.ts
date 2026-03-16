8c0cd53797da7b90e16bb4237bd695df495b1645
Mohamed Hamdy <99550272+MohamedHamada13@users.noreply.github.com>
2026-03-17 01:13:14 +0200

    Feat: Add a creative footer & update the navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Slider } from './components/slider/slider';
 import { ProductCard } from './components/product-card/product-card';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Slider, ProductCard],
+  imports: [Navbar, Slider, ProductCard, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
