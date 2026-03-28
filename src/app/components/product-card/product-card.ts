0319203462c4fe1c3c86a116ae35550896d1df9e
Mohamed Hamdy <99550272+MohamedHamada13@users.noreply.github.com>
2026-03-28 08:16:57 +0200

    Improve: Day-3 task, make the products as dynamic by storing all products data in productcard.ts file in array

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
-export class ProductCard {}
+export class ProductCard {
+  products = [
+    { id: 1, name: 'Smart Watch', price: 120, image: 'b-sunglass-sm.jpg', inStock: true },
+    { id: 2, name: 'Classic Watch', price: 90, image: 'febreze-sm.jpg', inStock: false },
+    { id: 3, name: 'Sport Watch', price: 150, image: 'girl-shoes-sm.jpg', inStock: true },
+    { id: 4, name: 'Running Shoes', price: 120, image: 'w-sunglass-sm.jpg', inStock: true },
+    { id: 5, name: 'Casual Shoes', price: 100, image: 'y-atary-sm.jpg', inStock: false }
+  ];
+}
--- src/app/components/slider/slider.ts
+++ src/app/components/slider/slider.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './slider.html',
   styleUrl: './slider.css',
 })
-export class Slider {}
+export class Slider {
+  sliderImages = [
+    {id: 1, label: 'First Slide', description: 'Some representative placeholder content for the second slide.', image: 'img1.jpg'},
+    {id: 2, label: 'Second Slide', description: 'Some representative placeholder content for the second slide.', image: 'img2.jpg'},
+    {id: 3, label: 'Third Slide', description: 'Some representative placeholder content for the third slide.', image: 'img3.jpg'}
+  ]
+}
